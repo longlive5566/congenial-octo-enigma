85da509eb4184d2c0444cf196e9401a37a418985
longlive5566 <longlivewan@qq.com>
2026-04-26 09:44:17 +0800

    Simplify project for successful build

--- app/src/main/java/com/winclaw/bookrank/BookRankApplication.kt
+++ app/src/main/java/com/winclaw/bookrank/BookRankApplication.kt
@@ -1,23 +1,15 @@
 package com.winclaw.bookrank
 
 import android.app.Application
-import com.winclaw.bookrank.data.worker.RankScheduler
 
 /**
  * 应用入口
  */
 class BookRankApplication : Application() {
     
-    companion object {
-        lateinit var instance: BookRankApplication
-            private set
-    }
-    
     override fun onCreate() {
         super.onCreate()
-        instance = this
-        
-        // 初始化定时任务（可选）
-        // RankScheduler(this).schedulePeriodicUpdate()
+        // 初始化数据库
+        val database = data.local.AppDatabase.getDatabase(this)
     }
 }
